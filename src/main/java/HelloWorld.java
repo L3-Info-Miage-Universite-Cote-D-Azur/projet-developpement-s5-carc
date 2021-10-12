c3ed2aa23ab2a446dee5553556fd9b3946b7a9c3
Sébastien Aglaé <sebastien.aglae@etu.unice.fr>
2021-10-12 16:46:27 +0200

    GameConfigLoadingTest

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,5 +1,11 @@
+import logic.Game;
+import logic.config.GameConfig;
+
+import java.io.IOException;
+
 public class HelloWorld {
-    public static void main(String[] arg){
+    public static void main(String[] arg) {
         System.out.println("Hello World !");
+        System.out.println(new GameConfig().toString());
     }
 }
