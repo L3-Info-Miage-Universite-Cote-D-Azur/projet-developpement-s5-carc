f92bd1a1d46a8b0ff090dcf56e259be81e37c92c
Loïc Le Contel <37910983+Ekorion@users.noreply.github.com>
2021-10-12 15:22:31 +0200

    #2 import vecteur2

--- src/main/java/logic/board/GameBoard.java
+++ src/main/java/logic/board/GameBoard.java
@@ -1,5 +1,6 @@
 package logic.board;
 
+import logic.math.Vecteur2;
 import logic.tile.Tile;
 
 import java.util.ArrayList;
@@ -14,11 +15,11 @@ public class GameBoard {
 
     }
 
-    public Tile getTileAt(Vecteur Vector2){
+    public Tile getTileAt(Vecteur2 Vector2){
         return null;
     }
 
-    public boolean hasTileAt(Vecteur Vector2){
+    public boolean hasTileAt(Vecteur2 Vector2){
         return false;
     }
 }
