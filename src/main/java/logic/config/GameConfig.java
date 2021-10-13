bae7531e0ad9b842e1d75c7d5a4fbe60b387081a
Sébastien Aglaé <sebastien.aglae@etu.unice.fr>
2021-10-13 17:03:16 +0200

    Fix jackson & validate method in GameConfig

--- src/main/java/logic/config/GameConfig.java
+++ src/main/java/logic/config/GameConfig.java
@@ -3,27 +3,36 @@ package logic.config;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import logic.tile.TileType;
 
-import java.util.HashMap;
+import java.util.*;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
-public class GameConfig {
-    public class TileConfig {
-        public int DECK_COUNT;
-    }
+class TileConfig {
+    public int DECK_COUNT;
+}
 
+public class GameConfig {
     public int MIN_PLAYERS = 2;
     public int MAX_PLAYERS = 5;
     public int PLAYER_DECK_CAPACITY;
-    public HashMap<TileType, TileConfig> TILES = new HashMap<>() {{
-        // TODO
-    }};
+    public HashMap<TileType, TileConfig> TILES = new HashMap<>();
 
     public boolean validate() {
         if (MIN_PLAYERS < 0 || MIN_PLAYERS > MAX_PLAYERS)
             return false;
         if (PLAYER_DECK_CAPACITY < 1)
             return false;
+        List<TileType> tileTypes = new ArrayList<>(List.of(TileType.values()));
+        for (Map.Entry<TileType, TileConfig> tile : TILES.entrySet()) {
+            System.out.println(tileTypes);
+            if (tileTypes.contains(tile.getKey()))
+                tileTypes.remove(tile.getKey());
+            else
+                return false;
+
+            if (tile.getValue().DECK_COUNT < 0)
+                return false;
+        }
 
         return true;
     }
