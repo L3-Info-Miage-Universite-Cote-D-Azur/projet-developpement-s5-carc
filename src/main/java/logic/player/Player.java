aca39f73104b5a53c6903f5ad0913ef0b6557962
Loïc Le Contel <37910983+Ekorion@users.noreply.github.com>
2021-10-13 11:56:48 +0200

    Deck removed

--- src/main/java/logic/player/Player.java
+++ src/main/java/logic/player/Player.java
@@ -1,17 +1,14 @@
 package logic.player;
 
 import input.PlayerInput;
-import logic.tile.TileDeck;
 
 public class Player {
     private final PlayerInfo info;
     private final PlayerInput input;
-    private final TileDeck deck;
 
     public Player(PlayerInfo info, PlayerInput input) {
         this.info = info;
         this.input = input;
-        this.deck = new TileDeck();
     }
 
     public void onTurn() {
