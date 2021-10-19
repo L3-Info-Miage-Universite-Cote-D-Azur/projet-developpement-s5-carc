c0ce9702462c75ab5abf61d4ae2ee1a41dd3bab2
Lucxs <lucasb0695@gmail.com>
2021-10-19 14:47:27 +0200

    Update Player.java #18

--- src/main/java/logic/player/Player.java
+++ src/main/java/logic/player/Player.java
@@ -3,18 +3,29 @@ package logic.player;
 import input.PlayerInput;
 import logic.Game;
 
-public class Player {
+public abstract class Player {
     private final PlayerInfo info;
     private final PlayerInput input;
     private final Game game;
+    private int score;
 
     public Player(PlayerInfo info, PlayerInput input, Game game) {
         this.info = info;
         this.input = input;
         this.game = game;
+        this.score = score;
     }
 
     public void onTurn() {
         input.onTurn();
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public int addScore() {
+        this.score = score;
+        return score;
     }
 }
