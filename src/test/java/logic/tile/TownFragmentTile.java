ade512f517434ec374daf876fbfde54c4329626e
Lucxs <lucasb0695@gmail.com>
2021-10-26 14:22:45 +0200

    Implementation of TownFragmentTile.java

--- src/test/java/logic/tile/TownFragmentTile.java
+++ src/test/java/logic/tile/TownFragmentTile.java
@@ -3,6 +3,15 @@ package logic.tile;
 import logic.math.Vector2;
 
 public class TownFragmentTile extends Tile{
-    
+
+    public TownFragmentTile(TileData data, Vector2 position) {
+        super(data, position);
+    }
+
+    @Override
+    public TileType getType() {
+        return null;
+    }
+
 }
 
