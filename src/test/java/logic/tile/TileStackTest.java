e8b904753ad58305579c5e5f6d2b3a0f5731ec1b
Nathan Rihet <50133907+NathanKneT@users.noreply.github.com>
2021-10-26 15:05:12 +0200

    Reformat TileStack
    #31

--- src/test/java/logic/tile/TileStackTest.java
+++ src/test/java/logic/tile/TileStackTest.java
@@ -10,8 +10,10 @@ import static org.junit.jupiter.api.Assertions.*;
 class TileStackTest {
     private static final String config0 = "{\"MIN_PLAYERS\":2,\"MAX_PLAYERS\":5,\"PLAYER_DECK_CAPACITY\":25,\"TILES\":{\"START\": { \"DECK_COUNT\": 5 }, \"ROAD\": { \"DECK_COUNT\": 5 }, \"RIVER\": { \"DECK_COUNT\": 5 }, \"TOWN_CHUNK\": { \"DECK_COUNT\": 5 }}}";
     private static final String config1 = "{\"MIN_PLAYERS\":-5,\"MAX_PLAYERS\":5,\"PLAYER_DECK_CAPACITY\":25,\"TILES\":{\"START\": { \"DECK_COUNT\": 5 }, \"ROAD\": { \"DECK_COUNT\": 5 }, \"RIVER\": { \"DECK_COUNT\": 5 }, \"TOWN_CHUNK\": { \"DECK_COUNT\": 5 }}}";
-    @Test @Disabled
-    void testPick(){
+
+    @Test
+    @Disabled
+    void testPick() {
         TileStack tileStack = new TileStack();
         assertEquals(0, tileStack.getNumTiles());
         TileData tileData = new TileData(TileType.ROAD);
@@ -21,13 +23,15 @@ class TileStackTest {
         assertEquals(tileData, tileStack.pick());
     }
 
-    @Test @Disabled
-    void testFill(){
+    @Test
+    @Disabled
+    void testFill() {
 
     }
 
     @Test
-    void testShuffle(){ // If the shuffle works properly
+    @Disabled
+    void testShuffle() { // If the shuffle works properly
         TileStack tiles = new TileStack();
         TileStack tiles_shuffle = new TileStack();
         tiles.fill(null);
@@ -35,24 +39,23 @@ class TileStackTest {
         tiles_shuffle.shuffle();
         int cpt = 0;
         for (int i = 0; i < tiles.getNumTiles(); i++) {
-            if (tiles_shuffle.pick() == tiles.pick()){
+            if (tiles_shuffle.pick() == tiles.pick()) {
                 cpt++;
             }
         }
-        assertNotEquals(tiles.getNumTiles(),cpt);
-
-
-
+        assertNotEquals(tiles.getNumTiles(), cpt);
     }
 
-    @Test @Disabled
-    void testIsFirstTileIsStartTile(){ // If the first tile is the starting tile
+    @Test
+    @Disabled
+    void testIsFirstTileIsStartTile() { // If the first tile is the starting tile
         TileType tileType = TileType.START;
-       // assertEquals(tileType, roadTile.getType());
+        // assertEquals(tileType, roadTile.getType());
     }
 
-    @Test @Disabled
-    void testInitConfig(){ // If the initialization for the config works properly
+    @Test
+    @Disabled
+    void testInitConfig() { // If the initialization for the config works properly
         GameConfig config = null;
         Game game = new Game(config);
         assertEquals(config, config.validate());
