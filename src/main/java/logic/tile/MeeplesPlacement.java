e6ceb1a6005fdb01bd6a14f31963a08cca0d272f
Nathan Rihet <50133907+NathanKneT@users.noreply.github.com>
2021-11-09 14:44:11 +0100

    Create MeeplePlacement
    #38

---
+++ src/main/java/logic/tile/MeeplesPlacement.java
@@ -0,0 +1,13 @@
+package logic.tile;
+
+public class MeeplesPlacement {
+/*
+Verifier présence de tuile avec l'utilisation des vecteurs
+
+Si on place un meeple c'est boolean car on peut avoir soit un meeple exemple
+meeple.yellow = 0
+meeple.red = 1
+meeple.green = 0
+
+ */
+}
