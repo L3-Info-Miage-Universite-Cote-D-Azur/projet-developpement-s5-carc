1fabe8f41db6bd70e0ccb230183abcb98bbde4ed
Lucxs <lucasb0695@gmail.com>
2021-11-09 15:47:29 +0100

    Create MeepleTest.java
    #38

---
+++ src/test/java/logic/tile/MeeppleTest.java
@@ -0,0 +1,32 @@
+package logic.tile;
+
+import logic.Game;
+import logic.board.GameBoard;
+import logic.config.GameConfig;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+public class MeeppleTest {
+
+    /* check if 2 meeples of the same team are on the board*/
+    @Test
+    void MeeplesTeams(){
+
+    }
+
+    /* check if 2 meeples are on the same tile*/
+    @Test
+    void MeeplesTiles(){
+
+    }
+
+    /* check if the tile is already on the board before placing a meeple on it*/
+    @Test
+    void TilesCreated(){
+
+    }
+
+
+
+}
