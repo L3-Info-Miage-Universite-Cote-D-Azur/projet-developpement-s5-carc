12425f983bb543c01d5854b56fdc993b1632b28a
Mike Chiappe <33669760+Mimi8298@users.noreply.github.com>
2021-11-09 15:35:42 +0100

    ChunkConfig and missing files added #42

--- src/main/java/logic/config/GameConfig.java
+++ src/main/java/logic/config/GameConfig.java
@@ -5,32 +5,23 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import java.util.*;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import logic.tile.Chunk;
+import logic.tile.ChunkType;
+import logic.tile.Tile;
 
 public class GameConfig {
     public int MIN_PLAYERS = 2;
     public int MAX_PLAYERS = 5;
-    public int PLAYER_DECK_CAPACITY;
-    public HashMap<TileType, TileConfig> TILES = new HashMap<>() {{
-        put(TileType.START, new TileConfig() {{ DECK_COUNT = 1; }});
-        put(TileType.ROAD, new TileConfig() {{ DECK_COUNT = 10; }});
-        put(TileType.TOWN_CHUNK, new TileConfig() {{DECK_COUNT = 10; }});
-        put(TileType.ABBEY, new TileConfig() {{DECK_COUNT = 6; }});
-    }};
+    public ArrayList<TileConfig> TILES = new ArrayList<>();
 
     public boolean validate() {
         if (MIN_PLAYERS < 0 || MIN_PLAYERS > MAX_PLAYERS)
             return false;
-        if (PLAYER_DECK_CAPACITY < 1)
-            return false;
-        List<TileType> tileTypes = new ArrayList<>(List.of(TileType.values()));
-        for (Map.Entry<TileType, TileConfig> tile : TILES.entrySet()) {
-            if (tileTypes.contains(tile.getKey()))
-                tileTypes.remove(tile.getKey());
-            else
-                return false;
 
-            if (tile.getValue().DECK_COUNT < 0)
+        for (TileConfig tile : TILES) {
+            if (!tile.validate()) {
                 return false;
+            }
         }
 
         return true;
