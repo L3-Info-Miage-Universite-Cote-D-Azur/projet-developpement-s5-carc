2c981bedce10630cb6f8fba9c092b8107cfef7f4
Loïc Le Contel <37910983+Ekorion@users.noreply.github.com>
2021-11-10 12:25:45 +0100

    Restictions placement tile #37

--- src/main/java/logic/tile/ChunkType.java
+++ src/main/java/logic/tile/ChunkType.java
@@ -1,10 +1,28 @@
 package logic.tile;
 
 public enum ChunkType {
-    ROAD,
-    TOWN,
-    TOWN_WALL,
-    RIVER,
-    ABBEY,
-    FIELD
+    ROAD(0, 0),
+    TOWN(1, 1, 2),
+    TOWN_WALL(2, 1, 2),
+    RIVER(3, 3),
+    ABBEY(4, 4),
+    FIELD(5, 5);
+
+    private final int type;
+    private final int[] compatibleTypes;
+
+    ChunkType(int type, int... compatibleChunks) {
+        this.type = type;
+        this.compatibleTypes = compatibleChunks;
+    }
+
+    public final boolean isCompatibleWith(ChunkType type) {
+        for (int compatibleType : compatibleTypes) {
+            if (compatibleType == type.type) {
+                return true;
+            }
+        }
+
+        return false;
+    }
 }
