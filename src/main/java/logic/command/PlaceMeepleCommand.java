f644fd431410b482c0e3b676bb11997c394eab8f
Loïc Le Contel <37910983+Ekorion@users.noreply.github.com>
2021-11-10 14:29:03 +0100

    Placement of meeple #38

---
+++ src/main/java/logic/command/PlaceMeepleCommand.java
@@ -0,0 +1,43 @@
+package logic.command;
+
+import logger.Logger;
+import logic.Game;
+import logic.meeple.Meeple;
+import logic.player.PlayerBase;
+import logic.tile.Chunk;
+import logic.tile.ChunkOffset;
+import logic.tile.Tile;
+
+public class PlaceMeepleCommand implements ICommand {
+    private final Tile tile;
+    private final ChunkOffset chunkOffset;
+    private final PlayerBase executor;
+
+    public PlaceMeepleCommand(Tile tile, ChunkOffset offset, PlayerBase executor) {
+        this.tile = tile;
+        this.chunkOffset = offset;
+        this.executor = executor;
+    }
+
+    @Override
+    public boolean execute(Game game) {
+        if (executor.getRemainingMeepleCount() < 1) {
+            Logger.warning("Player %d has no meeple to place.", executor.getId());
+            return false;
+        }
+
+        Chunk chunk = tile.getChunk(chunkOffset);
+
+        if (chunk.hasMeeple()) {
+            Logger.warning("Try to place meeple on a chunk that already have meeple.");
+            return false;
+        }
+
+        Logger.info("Player %d places meeple at tile %s, chunk %d", tile.getPosition(), chunkOffset);
+
+        chunk.setMeeple(new Meeple(executor));
+        executor.removeRemainingMeepleCount();
+
+        return true;
+    }
+}
