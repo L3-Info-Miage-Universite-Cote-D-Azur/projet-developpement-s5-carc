13f07c749381a0c9aae3df31d1c92ea2cfbb914d
Lucxs <lucasb0695@gmail.com>
2021-11-10 11:49:06 +0100

    Update MeepleTest.java
    #38

--- src/test/java/logic/tile/MeepleTest.java
+++ src/test/java/logic/tile/MeepleTest.java
@@ -4,14 +4,17 @@ import logic.Game;
 import logic.board.GameBoard;
 import logic.config.GameConfig;
 import logic.math.Vector2;
+import logic.meeple.Meeple;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
-public class MeeppleTest {
+public class MeepleTest {
 
     /* check if 2 meeples of the same team are on the board*/
+    @Disabled
     @Test
     void MeeplesTeams(){
 
@@ -20,6 +23,8 @@ public class MeeppleTest {
     /* check if 2 meeples are on the same tile*/
     @Test
     void MeeplesTiles(){
+        
+
 
     }
 
