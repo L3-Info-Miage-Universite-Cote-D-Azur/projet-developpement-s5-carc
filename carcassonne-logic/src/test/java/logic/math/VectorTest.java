b9081b2eaa4984697b5facce4561ebaf3da9207f
Nathan Rihet <50133907+NathanKneT@users.noreply.github.com>
2021-11-16 16:23:45 +0100

    Update VectorTest.java

--- carcassonne-logic/src/test/java/logic/math/VectorTest.java
+++ carcassonne-logic/src/test/java/logic/math/VectorTest.java
@@ -25,4 +25,28 @@ class VectorTest {
         assertEquals(vector0, vector2);
         assertNotEquals(vector0, null);
     }
+
+    @Test
+    void testGetX(){
+        int n = 30;
+        Vector2 vector0 = new Vector2(n, n * 2);
+        assertEquals(30, vector0.getX());
+    }
+
+    @Test
+    void testGetY(){
+        int n = 40;
+        Vector2 vector0 = new Vector2(n, n * 2);
+        assertEquals(80, vector0.getY());
+    }
+
+    @Test
+    void testAdd() {
+        int n = 50;
+        Vector2 vector0 = new Vector2(n, n * 2);
+        Vector2 vector1 = new Vector2(n * 2, n);
+        Vector2 vectorFinal = new Vector2(150, 150);
+        Vector2 vectorResult = vector1.add(vector0);
+        assertEquals(vectorResult, vectorFinal);
+    }
 }
