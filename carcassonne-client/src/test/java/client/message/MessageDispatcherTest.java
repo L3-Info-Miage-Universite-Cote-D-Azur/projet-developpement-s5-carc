90af43b8164b883cf8e9c8fb0be63121c149d01b
Mike Chiappe <33669760+Mimi8298@users.noreply.github.com>
2021-12-08 17:34:38 +0100

    Tests added for message dispatcher

---
+++ carcassonne-client/src/test/java/client/message/MessageDispatcherTest.java
@@ -0,0 +1,36 @@
+package client.message;
+
+import network.message.Message;
+import network.message.MessageType;
+import network.message.connection.ClientHelloMessage;
+import org.junit.jupiter.api.Test;
+
+public class MessageDispatcherTest {
+    @Test
+    void testDispatchToHandler() {
+        MessageDispatcher dispatcher = new MessageDispatcher(null);
+
+        final boolean[] handled = {false,false,false};
+
+        dispatcher.addHandler(new IMessageHandler() {
+            @Override
+            public void handleMessage(Message message) {
+                handled[0] = message.getType() == MessageType.CLIENT_HELLO;
+            }
+        });
+        dispatcher.addHandler(new IMessageHandler() {
+            @Override
+            public void handleMessage(Message message) {
+                handled[1] = message.getType() == MessageType.CLIENT_HELLO;
+            }
+        });
+        dispatcher.addHandler(new IMessageHandler() {
+            @Override
+            public void handleMessage(Message message) {
+                handled[2] = message.getType() == MessageType.CLIENT_HELLO;
+            }
+        });
+
+        dispatcher.handle(new ClientHelloMessage());
+    }
+}
