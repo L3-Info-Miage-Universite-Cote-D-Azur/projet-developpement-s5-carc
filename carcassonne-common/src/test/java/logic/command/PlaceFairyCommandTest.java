923217af7094e0c74d27eae3e8c69fdeffe0100b
Mike Chiappe <33669760+Mimi8298@users.noreply.github.com>
2021-12-08 11:06:48 +0100

    Test for PlaceFairyCommand

---
+++ carcassonne-common/src/test/java/logic/command/PlaceFairyCommandTest.java
@@ -0,0 +1,24 @@
+package logic.command;
+
+import logic.Game;
+import logic.TestUtils;
+import logic.board.GameBoard;
+import logic.meeple.Meeple;
+import logic.tile.chunk.ChunkId;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class PlaceFairyCommandTest {
+    @Test
+    void testPlacement() {
+        Game game = TestUtils.initGameEnv(2, false, true);
+
+        TestUtils.placeTileRandomly(game);
+
+        assertFalse(game.getCommandExecutor().execute(new PlaceFairyCommand(GameBoard.STARTING_TILE_POSITION, ChunkId.CENTER_MIDDLE)));
+        game.getBoard().getTileAt(GameBoard.STARTING_TILE_POSITION).getChunk(ChunkId.CENTER_MIDDLE).setMeeple(new Meeple(game.getTurnExecutor()));
+        assertTrue(game.getCommandExecutor().execute(new PlaceFairyCommand(GameBoard.STARTING_TILE_POSITION, ChunkId.CENTER_MIDDLE)));
+    }
+}
